7259516af2ebd80f96ebe10c78f80eb8f4c01ceb
Nilesh-navalkar <80153286+Nilesh-navalkar@users.noreply.github.com>
2023-05-11 11:28:40 -0700

    Update getNews.py

--- ScrapperJob/getNews.py
+++ ScrapperJob/getNews.py
@@ -4,40 +4,44 @@ import pandas
 
 
 class getNews:
+    def __init__(self):
+        self.d={}
+
     def get_news_from_toi(self):
-        url='https://timesofindia.indiatimes.com/news/'
         i=1
-        d={}
+        url='https://timesofindia.indiatimes.com/news/'
         while i<=8:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
             news=soup.select('#ulItemContainer ul li a')
             for n in news:
                 try:
-                    d['https://m.timesofindia.com'+n['href']]=n['title']
+                    self.d['https://m.timesofindia.com'+n['href']]=n['title']
                 except:
                     pass
             i=i+1
             url=url+'/'+str(i)
-        return pandas.DataFrame(d.items(), columns=['HeaderUrl','HeaderName'])
-
-
+        
     def get_news_from_ndtv(self):
-        url='https://www.ndtv.com/latest/'
         i=1
-        d={}
+        url='https://www.ndtv.com/latest/'
         while i<=8:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
             news=soup.select('.lisingNews .news_Itm .news_Itm-cont h2 a')
             for n in news:
                 try:
-                    d[n['href']]=n.text
+                    self.d[n['href']]=n.text
                 except:
                     pass
             i=i+1
             url=url+'/page-'+str(i)
-        return pandas.DataFrame(d.items(), columns=['HeaderUrl','HeaderName'])
     
-
+    def get_dataframe(self):
+        return pandas.DataFrame(self.d.items(), columns=['HeaderUrl','HeaderName'])
+    
+# o=getNews()
+# o.get_news_from_ndtv()
+# o.get_news_from_toi()
+# print(o.get_dataframe())
 
